21d303d59a7b5e5997797621e01213d641995947
Tomas Alejandro <39777824+talejandro@users.noreply.github.com>
2022-09-27 23:22:42 -0300

    init

---
+++ posicion.py
@@ -0,0 +1,21 @@
+from turtle import position
+from urllib.request import urlopen
+import json
+import time
+
+url = 'https://api.wheretheiss.at/v1/satellites/25544'
+
+loops = 2
+
+while loops != 1: 
+    req = urlopen(url)
+    response = req
+    data = json.loads(response.read())
+    position_iss = {
+                "Tiempo" : data['timestamp'],
+                "Latitud" : data['latitude'], 
+                "Longitud" : data['longitude'], 
+                "Velocidad" : data['velocity']}
+    with open('iss.json', 'w') as f:  
+        json.dump(position_iss, f)
+    time.sleep(5)
